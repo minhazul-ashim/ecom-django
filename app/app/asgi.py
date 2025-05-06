b8a135c9fac1ba6502ee738460c475a624559cfb
minhazul_ashim <minhazul.ashim@gmail.com>
2025-05-06 20:39:01 +0600

    account creation and login logout function done

--- app/app/asgi.py
+++ app/app/asgi.py
@@ -10,6 +10,11 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -10,6 +10,11 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
